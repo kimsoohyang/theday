5214ac63eb4a95fa5c88e61582e614a4b4bd05ae
kimsoohyang <145723586+kimsoohyang@users.noreply.github.com>
2023-11-02 10:51:07 +0900

    Add files via upload

---
+++ script/gallery2.js
@@ -0,0 +1,17 @@
+ $(function(){
+ var swiper = new Swiper('.gallery_inner2 ', {
+    slidesPerView: 3,//보여지는 갤러리 수
+    spaceBetween: 40,//갤러리 사이 간격
+    speed: 1500,//버튼을 슬라이드가 넘어가는 시간
+
+      loop: true,//슬라이드 무한반복
+      navigation: {//화살표 버튼
+        nextEl: '.gallery2 .swiper-button-next',
+        prevEl: '.gallery2 .swiper-button-prev',
+      },
+	    pagination: {//블릿 버튼
+        el: '.gallery2 .swiper-pagination',
+        clickable: true,
+      },
+    });
+});
